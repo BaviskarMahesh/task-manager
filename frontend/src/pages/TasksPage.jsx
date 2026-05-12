dbb3526ba1eed5ba202b89e421817d59ff95b254
Mahesh Baviskar <maheshbaviskar@Maheshs-MacBook-Air.local>
2026-05-12 23:14:58 +0530

    Practice

--- frontend/src/pages/TasksPage.jsx
+++ frontend/src/pages/TasksPage.jsx
@@ -3,7 +3,8 @@ import axios from 'axios';
 import TaskForm from '../components/TaskForm';
 import TaskList from '../components/TaskList';
 
-const API_URL = 'http://52.91.228.97:5001/api/tasks';
+// const API_URL = 'http://52.91.228.97:5001/api/tasks';
+const API_URL = 'http://localhost:5001/api/tasks';
 
 function TasksPage() {
   const [tasks, setTasks] = useState([]);
